a7a30dee66d315adcf9e534281d74e7a634d434c
naresh-barfa-au19 <nareshbarfa007@gmail.com>
2025-04-16 13:44:30 +0530

    remove mongo url fix issue

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   mongoose.connection.on('connected', () => console.log('Database Connected'))
-  await mongoose.connect(`${process.env.MONGODB_URI || MONGODB_URI}/BookMyDoctor`, {
+  await mongoose.connect(`${process.env.MONGODB_URI}/BookMyDoctor`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     ssl: true, // Enable SSL
