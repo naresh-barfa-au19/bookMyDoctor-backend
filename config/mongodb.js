543e4c29d233b8b20f55e7f79239805bcc3de5fd
naresh-barfa-au19 <nareshbarfa007@gmail.com>
2025-04-16 13:44:00 +0530

    remove mongo url

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = "mongodb+srv://nareshbarfa007:B4bDcTdHbeSTFSTo@cluster0.j3hovhl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
 const connectDB = async () => {
   mongoose.connection.on('connected', () => console.log('Database Connected'))
-  await mongoose.connect(`${process.env.MONGODB_URI || MONGODB_URI}/BookMyDoctor`)
+  await mongoose.connect(`${process.env.MONGODB_URI || MONGODB_URI}/BookMyDoctor`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true, // Enable SSL
+  })
 }
 
 export default connectDB
